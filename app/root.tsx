a017707b22f93e1a03a920b6828d29f98ca99ed1
Rogers Aaron <arumeru@gmail.com>
2025-06-13 05:49:05 +0300

    Added license key for syncfusion

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,7 @@ export const links: Route.LinksFunction = () => [
 ];
 
 import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(import.meta.env.VITE_EJ2_LICENSE_KEY);
 
 registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
